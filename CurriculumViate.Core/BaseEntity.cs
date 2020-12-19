15a8ce17be1fdc9617832883bfa8cf497fcddc83
huynhtruongdyu <huynhtruongdyu@gmail.com>
2020-12-19 12:15:37 +0700

    revert

--- CurriculumViate.Core/BaseEntity.cs
+++ CurriculumViate.Core/BaseEntity.cs
@@ -1,13 +1,23 @@
 using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace CurriculumViate.Core
 {
     public abstract class BaseEntity
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public int Id { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
         public DateTime CreateOnUtc { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
         public DateTime? UpdateOnUtc { get; set; }
-        public Status Status { get; set; }
+
+        public Status Status { get; set; 
+        
         public BaseEntity()
         {
             this.CreateOnUtc  = DateTime.UtcNow;
